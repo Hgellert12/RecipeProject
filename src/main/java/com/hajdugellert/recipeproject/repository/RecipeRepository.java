e8a8318981ac2d3740e8761b9c7987e59368ed41
Hgellert12 <hajdugellert00@gmail.com>
2026-04-16 22:28:14 +0200

    Rename recipeRepository to RecipeRepository

--- src/main/java/com/hajdugellert/recipeproject/repository/RecipeRepository.java
+++ src/main/java/com/hajdugellert/recipeproject/repository/RecipeRepository.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 @Repository
-public interface recipeRepository extends JpaRepository<Recipe, Long> {
+public interface RecipeRepository extends JpaRepository<Recipe, Long> {
     List<Recipe> findByNameIgnoringCase(String name);
     List<Recipe> findByCategoryIgnoringCase(String category);
     List<Recipe> findByFavorite(Boolean favorite);
